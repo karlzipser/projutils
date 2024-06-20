295fd2df945de952cae28aef40a1f09be88e5f25
Karl Zipser <karlzipser@berkeley.edu>
2024-06-20 14:06:23 -0700

    gacp

--- net_loss.py
+++ net_loss.py
@@ -13,6 +13,7 @@ class Loss_Recorder():
         sampletime=1,
         pct_to_show=100,
         s=0.1,
+        s_level=100,
         name='train loss',
     ):
         super(Loss_Recorder,self).__init__()
@@ -30,8 +31,11 @@ class Loss_Recorder():
     def add(self,d,external_ctr=None):
         self.acc+=d
         self.acc_ctr+=1
-        if not self.sampletimer.rcheck():
+        #if not self.sampletimer.rcheck():
+        #    return
+        if self.acc_ctr<5:
             return
+        #print(self.name,self.acc_ctr)
         d=self.acc/self.acc_ctr
         self.acc=0
         self.acc_ctr=0
@@ -42,8 +46,12 @@ class Loss_Recorder():
         self.i.append(self.ctr)
         self.t.append(time.time())
         self.f.append(d)
+
         if len(self.r):
-            s=self.s
+            #s=1-(1-self.s)/max(1,(self.s_level-self.ctr))
+            s=1-(1-self.s)*self.ctr/(self.s_level+self.ctr)
+            #print(s)
+            #s=self.s
             a=self.r[-1]
             b=(1-s)*a+s*d
             self.r.append(b)
@@ -61,10 +69,11 @@ class Loss_Recorder():
         d=lo(opj(self.path,get_safe_name(self.name)))
         for k in d:
             self.__dict__[k]=d[k]
-    def plot(self,clear=True,rawcolor='c',smoothcolor='b'):
-        if not self.plottimer.rcheck():
-            return
-        figure(self.name)
+    def plot(self,clear=True,rawcolor='c',smoothcolor='b',savefig=False):
+        #if not self.plottimer.rcheck():
+        #    return
+        #print(self.name)
+        figure('loss')
         if clear:
             clf()
         idx=int(len(self.i)*(100-self.pct_to_show)/100)
@@ -72,12 +81,13 @@ class Loss_Recorder():
         plot(self.i[idx:],self.r[idx:],smoothcolor,label=self.name)
         plt.xlabel('iterations');
         plt.ylabel('loss')
-        plt.title(d2s(self.name,self.path.replace(opjh(),'')))
-        spause()
-        plt.savefig(opj(self.path,get_safe_name(self.name)+'.pdf'))
+        plt.title('loss')#d2s(self.name,self.path.replace(opjh(),'')))
+        
+        if savefig:
+            plt.savefig(opj(self.path,'loss.pdf'))
     def do(self,d,external_ctr=None):
         self.add(d,external_ctr=external_ctr)
-        self.plot()
+        #self.plot()
         self.save()
     def current(self):
         return self.r[-1]

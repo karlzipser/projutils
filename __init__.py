36d0be5ed4cfe234fd21ee10f953b88e44d482c5
Karl Zipser <karlzipser@berkeley.edu>
2024-06-28 09:35:56 -0700

    gacp

--- __init__.py
+++ __init__.py
@@ -2,6 +2,7 @@ from utilz2 import *
 
 from .net_access import *
 from .net_loss import *
+from .net_loss2 import *
 from .net_stats import *
 
 #EOF
---
+++ net_loss2.py
@@ -0,0 +1,103 @@
+## 79 ########################################################################
+# branch master
+print(__file__)
+from utilz2 import *
+import torch
+
+class Loss_Recorder():
+    def __init__(
+        self,
+        path,
+        plottime=30,
+        savetime=30,
+        sampletime=10,
+        pct_to_show=100,
+        nsamples=30,
+        s=0.1,
+        s_level=100,
+        name='',
+        dataloader=None,
+        ):
+        super(Loss_Recorder,self).__init__()
+        packdict(self,locals())
+        self.f=[]
+        self.t=[]
+        self.i=[]
+        self.r=[]
+        self.outputs=[],
+        self.targets=[],
+        self.files=[],
+        self.indicies=[],
+        self.ctr=-1
+        self.plottimer=Timer(plottime)
+        self.savetimer=Timer(savetime)
+        self.sampletimer=Timer(sampletime)
+        self.acc_ctr=0
+        self.acc=0
+    def add(self,d,
+        external_ctr=None,outputs=None,targets=None):
+        self.acc+=d
+        self.acc_ctr+=1
+        if self.acc_ctr<self.nsamples:
+            return
+        d=self.acc/self.acc_ctr
+        self.acc=0
+        self.acc_ctr=0
+        if isNone(external_ctr):
+            self.ctr+=1
+        else:
+            self.ctr=external_ctr
+        self.i.append(self.ctr)
+        self.t.append(time.time())
+        self.f.append(d)
+        self.outputs.append(outputs)
+        self.targets.append(targets)
+
+        if len(self.r):
+            s=1-(1-self.s)*self.ctr/(self.s_level+self.ctr)
+            a=self.r[-1]
+            b=(1-s)*a+s*d
+            self.r.append(b)
+        else:
+            self.r.append(d)
+        self.save()
+    def save(self):
+        if not self.savetimer.rcheck():
+            return
+        d={}
+        for k in ['i','f','t','r','ctr']:
+            if 'timer' not in k:
+                d[k]=self.__dict__[k]
+        so(opj(self.path,get_safe_name(self.name)),d)
+    def load(self):
+        f=opj(self.path,get_safe_name(self.name)+'.pkl')
+        cg(f)
+        d=lo(f)
+        for k in d:
+            self.__dict__[k]=d[k]
+        cb('loaded',f)
+    def plot(self,clear=True,rawcolor='c',smoothcolor='b',savefig=False,fig='loss'):
+        figure(fig)
+        if clear:
+            clf()
+        idx=int(len(self.i)*(100-self.pct_to_show)/100)
+        plot(self.i[idx:],self.f[idx:],rawcolor)
+        plot(self.i[idx:],self.r[idx:],smoothcolor,label=self.name)
+        plt.xlabel('iterations');
+        plt.ylabel(self.name)
+        plt.title(self.name)
+        if savefig:
+            plt.savefig(opj(self.path,get_safe_name(fig+'.pdf')))
+    def calculate_state(self):
+        return dict(mean_accuracy=0.)
+    def current(self):
+        if len(self.r):
+            return self.r[-1]
+        else:
+            cE('warning')
+            kprint(self.__dict__)
+            return 0
+#eoc
+
+#EOF
+## 79 ########################################################################

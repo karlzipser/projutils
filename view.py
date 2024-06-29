d074d0b61f27ae5e1b1f7b434d62a6d246983a76
Karl Zipser <karlzipser@berkeley.edu>
2024-06-28 21:27:48 -0700

    gacp

--- view.py
+++ view.py
@@ -27,10 +27,14 @@ def merge_content(
   fs.reverse()
   
   tabdic=dict(
-      pdf=[],
-      png=[],
-      txt=[],
       py=[],
+      confusion=[],
+      scores=[],
+      accuracy=[],
+      txt=[],
+      loss=[],
+      examples=[],
+      outputs=[],
     )
   for f in fs:
     #cg(f)
@@ -110,9 +114,74 @@ def merge_content(
     if 'application/pdf' in div:
         pass#cy(div)
     if div:
-      tabdic[k]=div+'\n\n'
+      for k in kys(tabdic):
+        if k in f:
+          tabdic[k].append(div+'\n')
+          break
+  tabs="""
+  <div class="tab">\n"""
+  for k in tabdic:
+    tabs=tabs+"""<button class="tablinks" onclick="openTab(event, 'TAB')">TAB</button>\n""".replace('TAB',k)
+  tabs=tabs+"</div>\n"
 
-  hs=[css]+["""
+
+
+  tabscript="""
+<style>
+        .tab {
+            overflow: hidden;
+            border-bottom: 1px solid #ccc;
+        }
+        .tab button {
+            background-color: inherit;
+            border: none;
+            outline: none;
+            cursor: pointer;
+            padding: 14px 16px;
+            transition: 0.3s;
+            font-size: 17px;
+        }
+        .tab button:hover {
+            background-color: #ddd;
+        }
+        .tab button.active {
+            background-color: #ccc;
+        }
+        .tabcontent {
+            display: none;
+            padding: 6px 12px;
+            border-top: none;
+        }
+</style>
+
+<script>
+    function openTab(evt, tabName) {
+        var i, tabcontent, tablinks;
+
+        // Hide all tab content
+        tabcontent = document.getElementsByClassName("tabcontent");
+        for (i = 0; i < tabcontent.length; i++) {
+            tabcontent[i].style.display = "none";
+        }
+
+        // Remove the background color of all tab links
+        tablinks = document.getElementsByClassName("tablinks");
+        for (i = 0; i < tablinks.length; i++) {
+            tablinks[i].className = tablinks[i].className.replace(" active", "");
+        }
+
+        // Show the current tab content and add an "active" class to the button that opened the tab
+        document.getElementById(tabName).style.display = "block";
+        evt.currentTarget.className += " active";
+    }
+
+    // Set default tab to open
+    document.getElementsByClassName("tablinks")[0].click();
+</script>
+"""
+
+
+  hs=[css]+[tabscript]+[tabs]+["""
 <font face="Courier">
 <h3 id="copyText">"""+w.replace(opjh(),'')+"""</h3>
 </font>
@@ -135,8 +204,33 @@ def merge_content(
   });
 </script>
   """]
+  
   for k in tabdic:
-    hs+=[tabdic[k]]
+    tabcontent='\n'.join(tabdic[k])
+    hs+=["""
+<div id="THETAB" class="tabcontent">
+  THE_TAB_CONTENT
+</div>
+""".replace('THETAB',k).replace('THE_TAB_CONTENT',tabcontent)]
+
+
+  """
+  <div class="tab">
+    <button class="tablinks" onclick="openTab(event, 'Tab1')">Tab 1</button>
+    <button class="tablinks" onclick="openTab(event, 'Tab2')">Tab 2</button>
+    <button class="tablinks" onclick="openTab(event, 'Tab3')">Tab 3</button>
+  </div>
+
+  <div id="Tab2" class="tabcontent">
+    <h3>Tab 2</h3>
+    <p>This is the content for Tab 2.</p>
+  </div>
+
+  <div id="Tab3" class="tabcontent">
+    <h3>Tab 3</h3>
+    <p>This is the content for Tab 3.</p>
+  </div>
+  """
 
 
   #hs=[css]+[d2n('<font face="Courier"><h3>',w.replace(opjh(),''),
